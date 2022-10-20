0e280c23e032e7ca2b66267e089d397e30e7a2da
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2022-10-20 09:54:30 +0530

    distribute soln

--- contracts/Distribute.sol
+++ contracts/Distribute.sol
@@ -1,25 +1,39 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
 
-contract Distribute {
-    address[4] public contributors;
-    uint256 public createTime;
+contract OptimizedDistribute {
+    address immutable contributors_0;
+    address immutable contributors_1;
+    address immutable contributors_2;
+    address immutable contributors_3;
+    uint256 immutable createTime;
 
     constructor(address[4] memory _contributors) payable {
-        contributors = _contributors;
-        createTime = block.timestamp;
+        contributors_0 = _contributors[0];
+        contributors_1 = _contributors[1];
+        contributors_2 = _contributors[2];
+        contributors_3 = _contributors[3];
+        createTime = block.timestamp + 1 weeks;
     }
 
     function distribute() external {
-        require(
-            block.timestamp > createTime + 1 weeks,
-            "cannot distribute yet"
-        );
-
-        uint256 amount = address(this).balance / 4;
-        payable(contributors[0]).transfer(amount);
-        payable(contributors[1]).transfer(amount);
-        payable(contributors[2]).transfer(amount);
-        payable(contributors[3]).transfer(amount);
+        address temp_0 = contributors_0;
+        address temp_1 = contributors_1;
+        address temp_2 = contributors_2;
+        address temp_3 = contributors_3;
+        uint256 _createTime = createTime;
+        assembly {
+            mstore(returndatasize(), shr(2, selfbalance()))
+        }
+        assembly {
+            if gt(timestamp(), _createTime) {
+                // prettier-ignore
+                pop(call(gas(), temp_2, mload(returndatasize()), returndatasize(), returndatasize(), 
+                            call(gas(), temp_1, mload(returndatasize()), returndatasize(), returndatasize(), returndatasize(), returndatasize()), 
+                            call(gas(), temp_0, mload(returndatasize()), returndatasize(), returndatasize(), returndatasize(), returndatasize())))
+                selfdestruct(temp_3)
+            }
+        }
+        revert("cannot distribute yet");
     }
 }
